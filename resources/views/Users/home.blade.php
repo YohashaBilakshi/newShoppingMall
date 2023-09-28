61b19a24cb7289855b561b68368e646d4a41e2db
Sathindu Prabhath <113131510+Sathindu3@users.noreply.github.com>
2023-09-28 21:59:49 +0530

    footer

--- resources/views/Users/home.blade.php
+++ resources/views/Users/home.blade.php
@@ -18,9 +18,12 @@
         </div>
         <div id="h_items">
             <a href="#">Home</a>
-            <a href="#">Items</a>
+            <a href="#id_items">Items</a>
             <a href="#">Chart</a>
-            <a href="#">Login</a>
+            <a href="{{ asset('resources/views/Users/signup.blade.php') }}">Login</a>
+          
+
+          
         </div>
     </header>
 
@@ -113,6 +116,50 @@
     </section>
 
 </body>
+
+<footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>company</h4>
+                    <ul>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">our services</a></li>
+                        <li><a href="#">privacy policy</a></li>
+                        <li><a href="#">affiliate program</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>get help</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">shipping</a></li>
+                        <li><a href="#">returns</a></li>
+                        <li><a href="#">order status</a></li>
+                        <li><a href="#">payment options</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>online shop</h4>
+                    <ul>
+                        <li><a href="#">watch</a></li>
+                        <li><a href="#">bag</a></li>
+                        <li><a href="#">shoes</a></li>
+                        <li><a href="#">dress</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>follow us</h4>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </footer>
 
 <script src="{{ asset('assets/js/home.js') }}"></script>
 
